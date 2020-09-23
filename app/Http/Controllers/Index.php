b722e4233537e20f9239291ae4f60551fb573ec8
Nicolás Selman <nicolas.selman.25@gmail.com>
2020-09-23 19:32:20 -0300

    Cerrar sesión
    Botón a opción de cerrar sesión

--- app/Http/Controllers/Index.php
+++ app/Http/Controllers/Index.php
@@ -4,14 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\User;
 
 class Index extends Controller{
     public function loadIndex() {
+        $nombre = Auth::user()->nombres;
         if(!Auth::user()){
             return redirect('\login');
         }
         else{
-            return view('index');
+            return view('index', ['nombre' => $nombre]);
         }
     }
+
 }
