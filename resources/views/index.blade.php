7b77f3bd114bddcdff57ba5f83441865aa1984c5
José Ignacio Miquel <jomiquel@alumnos.uai.cl>
2020-09-23 20:24:43 -0300

    Limpieza de código
    cambié el nombre de ciertas variables y algunos textos

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,35 +5,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <title>Index</title>
+    <title>GestiónFIC - Index</title>
 </head>
 <body>
-    <h1>Hola {{ $nombre }} </h1>
-    <!-- Button trigger modal -->
-    <button type="button" class="btn btn-primary btn-danger" data-toggle="modal" data-target="#exampleModal">
+    <h1>Bienvenido/a {{ $nombre }}, te deseamos un magnífico día.</h1>
+    <button type="button" class="btn btn-primary btn-danger" data-toggle="modal" data-target="#logout">
         Cerrar Sesión
     </button>
-  
-    <!-- Modal -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal fade" id="logout" tabindex="-1" role="dialog" aria-labelledby="logoutLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Cerrar Sesión</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Cerrar Sesión</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                    </div>
+                    <div class="modal-body">
+                        <p>¿Realmente desea cerrar sesión?</p>
+                        <p>Para cerrar sesión haga click en cerrar sesión o haga click en cancelar para mantener sus sesión iniciada.</p>
+                    </div>
+                    <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                    <button type="button" class="btn btn-danger" onclick="location.href='{{ action('Login@logout') }}'">
+                        Cerrar sesión
+                    </button>
+                </div>
             </div>
-            <div class="modal-body">
-                ¿Seguro/a que desea salir?
-            </div>
-            <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Volver</button>
-            <button type="button" class="btn btn-danger" onclick="location.href='{{ action('Login@logout') }}'">
-                Cerrar sesión
-            </button>
-            </div>
-        </div>
         </div>
     </div>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
