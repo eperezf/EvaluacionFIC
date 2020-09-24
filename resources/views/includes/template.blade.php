fea98ba0e414231e69296ef203161373a3c60d71
Nicolás Selman <nicolas.selman.25@gmail.com>
2020-09-23 22:50:08 -0300

    Iconos en Navbar de "Font Awesome"
    Falta desarrollo en la posición de los botones

--- resources/views/includes/template.blade.php
+++ resources/views/includes/template.blade.php
@@ -4,38 +4,50 @@
     <meta charset="utf-8">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <script src="https://kit.fontawesome.com/7041ce19da.js" crossorigin="anonymous"></script>
     <title>EvalFIC | @yield('title')</title>
   </head>
+
   <body>
+
     <nav class="navbar navbar-light bg-light">
-      <a class="navbar-brand col-1" href="#">Navbar</a>
-      <button type="button" class="btn btn-primary btn-danger col-2 order-12" data-toggle="modal" data-target="#logout">
-        Cerrar Sesión
+      <a class="navbar-brand col-1 order-1" href="#">Navbar</a>
+
+      <button class="col-1 order-2 bg-primary" onclick="location.href='#'">
+        <span style="color: black;"><i class="far fa-calendar-alt fa-2x"></i></span>
       </button>
-      <div class="modal fade" id="logout" tabindex="-1" role="dialog" aria-labelledby="logoutLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel">¿Desea cerrar sesión?</h5>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
-              <button type="button" class="btn btn-danger" onclick="location.href='{{ action('Login@logout') }}'">
-                Cerrar sesión
-              </button>
-            </div>
+
+      <button type="button" class="btn btn-primary btn-danger col-1 order-12" data-toggle="modal" data-target="#logout">
+        <span><i class="fas fa-power-off fa-lg"></i></span>
+      </button>
+    </nav>
+
+    <div class="modal fade" id="logout" tabindex="-1" role="dialog" aria-labelledby="logoutLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">¿Desea cerrar sesión?</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+            <button type="button" class="btn btn-danger" onclick="location.href='{{ action('Login@logout') }}'">
+              Cerrar sesión
+            </button>
           </div>
         </div>
       </div>
-    </nav>
+    </div>
+
     <div class="container mt-2">
       @yield('contenido')
     </div>
+
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
   </body>
+
 </html>
