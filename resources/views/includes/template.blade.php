c060b538db316afe7a377d22e6456c6b76366573
José Ignacio Miquel <jomiquel@alumnos.uai.cl>
2020-09-23 21:34:57 -0300

    Mover logout a layout
    Saqué el botón de cerrar sesión de index.blade.php y lo puse en el layout

--- resources/views/includes/template.blade.php
+++ resources/views/includes/template.blade.php
@@ -8,7 +8,28 @@
   </head>
   <body>
     <nav class="navbar navbar-light bg-light">
-      <a class="navbar-brand" href="#">Navbar</a>
+      <a class="navbar-brand col-1" href="#">Navbar</a>
+      <button type="button" class="btn btn-primary btn-danger col-2 order-12" data-toggle="modal" data-target="#logout">
+        Cerrar Sesión
+      </button>
+      <div class="modal fade" id="logout" tabindex="-1" role="dialog" aria-labelledby="logoutLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">¿Desea cerrar sesión?</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+              <button type="button" class="btn btn-danger" onclick="location.href='{{ action('Login@logout') }}'">
+                Cerrar sesión
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </nav>
     <div class="container mt-2">
       @yield('contenido')
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,25 +3,4 @@
 @section('title', 'Inicio')
 @section('contenido')
   <h1>Bienvenido/a {{ $nombre }}.</h1>
-  <button type="button" class="btn btn-primary btn-danger" data-toggle="modal" data-target="#logout">
-    Cerrar Sesión
-  </button>
-  <div class="modal fade" id="logout" tabindex="-1" role="dialog" aria-labelledby="logoutLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">¿Desea cerrar sesión?</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
-          <button type="button" class="btn btn-danger" onclick="location.href='{{ action('Login@logout') }}'">
-            Cerrar sesión
-          </button>
-        </div>
-      </div>
-    </div>
-  </div>
 @endsection
