1d25160b3c18cd886638c1653b50447d96f2ee9f
José Ignacio Miquel <jomiquel@alumnos.uai.cl>
2020-10-10 19:39:18 -0300

    Formularios para agregar area y cargo en sus respectivas rutas
    Se hicieron los formularios para agregar cargos y areas. Los formularios están basados en los fillables que se declaran en los modelos de cada uno.

--- resources/views/area.blade.php
+++ resources/views/area.blade.php
@@ -2,6 +2,21 @@
 
 @section('title', 'Panel de Administración')
 @section('contenido')
-  <h1>Panel de Administración</h1>
-  <h2>Area</h2>
+<div id="menu">
+  <h1>Panel de Áreas</h1>
+  <a href="#agregar">Añadir Área</a>
+  <a href="#modificar">Modificar Área</a>
+</div><hr>
+<section id="agregar" name="Añadir Area">
+  <form action="" id="agregarArea">
+    <h3>Añadir una Área</h3>
+    <input name="area" placeholder="Área" type="text" id="area"><br>
+    <a href="" class="btn btn-primary">Añadir Área</a>
+  </form>
+</section><hr>
+<section id="modificar" name="Modificar Area">
+  <h3>Modificar Área</h3>
+
+</section>
+
 @endsection
--- resources/views/cargoAdministrativo.blade.php
+++ resources/views/cargoAdministrativo.blade.php
@@ -2,6 +2,19 @@
 
 @section('title', 'Panel de Administración')
 @section('contenido')
-  <h1>Panel de Administración</h1>
-  <h2>Cargo Administrativo</h2>
+  <div id="menu">
+    <h1>Panel de Cargos Administrativos</h1>
+    <a href="#agregar">Añadir Cargo</a>
+    <a href="#modificar">Modificar Cargo</a>
+  </div><hr>
+  <section id="agregar" name="Añadir Cargo">
+    <form action="" id="agregarCargo">
+      <h3>Añadir una Cargo</h3>
+      <input name="nombre" placeholder="Nombre del Cargo" type="text" id="nombre"><br>
+      <input name="peso" type="number" id="peso" placeholder="Peso"><br>
+      <a href="" class="btn btn-primary">Añadir Cargo</a>
+    </form>
+  </section><hr>
+  <section id="modificar" name="Modificar Cargo">
+    <h3>Modificar Cargo</h3>
 @endsection
