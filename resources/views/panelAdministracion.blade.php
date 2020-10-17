dc7057003cf9401ebc26646d8c7c69ff45a2a334
Nicolas Selman <nicolas.selman.25@gmail.com>
2020-10-17 13:27:22 -0300

    Base del diseño 'Panel Administración'
    Faltan detalles como ajustar bien las tarjetas para que todas tengan un tamaño igual fijo

--- resources/views/panelAdministracion.blade.php
+++ resources/views/panelAdministracion.blade.php
@@ -2,14 +2,98 @@
 
 @section('title', 'Panel de Administración')
 @section('contenido')
-  <h1>Panel de Administración</h1>
-  <a class="btn-link" href="{{route('area')}}">Modificar área</a><br>
-  <a class="btn-link" href="{{route('subarea')}}">Modificar subarea</a><br>
-  <a class="btn-link" href="{{route('curso')}}">Modificar curso</a><br>
-  <a class="btn-link" href="{{route('cargoAdministrativo')}}">Modificar cargo administrativo</a><br>
-  <a class="btn-link" href="{{route('publicacion')}}">Modificar publicación</a><br>
-  <a class="btn-link" href="{{route('asignatura')}}">Modificar asignatura</a><br>
-  <a class="btn-link" href="{{route('tutoria')}}">Modificar tutoría</a><br>
-  <a class="btn-link" href="{{route('actividad')}}">Modificar Actividad</a><br>
-  <a class="btn-link" href="{{route('tipoActividad')}}">Modificar Tipo de Actividad</a><br>
-  @endsection
+  <h1>Panel de Administración</h1><hr>
+  <div class="d-flex flex-wrap">
+    <div class="col-auto">
+      <div class="card mb-4">
+        <div class="card-body">
+          <h4 class="card-title">Áreas</h4>
+          <p class="card-text">Agrega o modifica áreas</p>
+          <a class="btn btn-primary" href="{{ route('agregarArea') }}">Agregar</a>
+          <a class="btn btn-secondary" href="#">Modificar</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-auto">
+      <div class="card mb-4">
+        <div class="card-body">
+          <h4 class="card-title">Subareas</h4>
+          <p class="card-text">Agrega o modifica subareas</p>
+          <a class="btn btn-primary" href="{{ route('agregarSubarea') }}">Agregar</a>
+          <a class="btn btn-secondary" href="#">Modificar</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-auto">
+      <div class="card mb-4">
+        <div class="card-body">
+          <h4 class="card-title">Cursos</h4>
+          <p class="card-text">Agrega o modifica cursos</p>
+          <a class="btn btn-primary" href="{{ route('agregarCurso') }}">Agregar</a>
+          <a class="btn btn-secondary" href="#">Modificar</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-auto">
+      <div class="card mb-4">
+        <div class="card-body">
+          <h4 class="card-title">Cargos Administrativos</h4>
+          <p class="card-text">Agrega o modifica cargos</p>
+          <a class="btn btn-primary" href="{{ route('agregarCargo') }}">Agregar</a>
+          <a class="btn btn-secondary" href="#">Modificar</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-auto">
+      <div class="card mb-4">
+        <div class="card-body">
+          <h4 class="card-title">Publicaciones</h4>
+          <p class="card-text">Agrega o modifica publicaciones</p>
+          <a class="btn btn-primary" href="{{ route('agregarPublicacion') }}">Agregar</a>
+          <a class="btn btn-secondary" href="#">Modificar</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-auto">
+      <div class="card mb-4">
+        <div class="card-body">
+          <h4 class="card-title">Asignaturas</h4>
+          <p class="card-text">Agrega o modifica asignaturas</p>
+          <a class="btn btn-primary" href="{{ route('agregarAsignatura') }}">Agregar</a>
+          <a class="btn btn-secondary" href="#">Modificar</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-auto">
+      <div class="card mb-4">
+        <div class="card-body">
+          <h4 class="card-title">Tutorías</h4>
+          <p class="card-text">Agrega o modifica tutorías</p>
+          <a class="btn btn-primary" href="{{ route('agregarTutoria') }}">Agregar</a>
+          <a class="btn btn-secondary" href="#">Modificar</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-auto">
+      <div class="card mb-4">
+        <div class="card-body">
+          <h4 class="card-title">Actividades</h4>
+          <p class="card-text">Agrega o modifica actividades</p>
+          <a class="btn btn-primary" href="{{ route('agregarActividad') }}">Agregar</a>
+          <a class="btn btn-secondary" href="#">Modificar</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-auto">
+      <div class="card mb-4">
+        <div class="card-body">
+          <h4 class="card-title">Tipos de Actividades</h4>
+          <p class="card-text">Agrega o modifica tipos de actividad</p>
+          <a class="btn btn-primary" href="{{ route('agregarTipoActividad') }}">Agregar</a>
+          <a class="btn btn-secondary" href="#">Modificar</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+@endsection
