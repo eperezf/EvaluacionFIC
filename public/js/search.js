b532eb4079eb0b050137f47e7755460c317709c2
Eduardo Pérez <eduardojoseperezf@gmail.com>
2020-10-24 18:20:02 -0300

    Buscador automático editor
    El buscador revisa el string y devuelve la lista de items que se parecen.
    
    TODO: Botones de edición, diseño y demás rutas.

---
+++ public/js/search.js
@@ -0,0 +1,31 @@
+var timer;
+var lista;
+var string;
+$( "#search" ).on('input', function( event ){
+  string = $("#search").val();
+  if (string != ""){
+    clearTimeout(timer);
+    $("#sugerencias").empty();
+    timer = setTimeout(alertFunc, 500);
+  }
+});
+
+
+
+function alertFunc() {
+  if ($("#search").val() == ""){
+    string = "0";
+  }
+  var request = new Request('/api/'+ruta+'/'+string);
+  fetch(request).then(function(response) {
+    return response.json();
+  }).then(function(res) {
+    lista = res;
+    if(!lista[0]){
+      $('#sugerencias').append('<div>No hay resultados</div>');
+    }
+    lista.forEach((item, i) => {
+      $('#sugerencias').append('<div>'+item.nombre+'</div>');
+    });
+  });
+}
