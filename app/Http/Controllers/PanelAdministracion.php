87046b37c256fda702a2fd6e72b21f853b5fcfd4
Nicolás Selman <nicolas.selman.25@gmail.com>
2020-10-30 11:16:35 -0300

    Submit data a BBDD Area con validacion
    Esto se realizo solamente en Área. Se validan los datos formateados de manera simple, luego se publican a la BBDD según como el usuario ingreso los datos de la nueva área

--- app/Http/Controllers/PanelAdministracion.php
+++ app/Http/Controllers/PanelAdministracion.php
@@ -29,6 +29,55 @@ use App\Http\Requests\StoreProyectoConcursable;
 
 class PanelAdministracion extends Controller
 {
+//Funciones generales
+
+    private function deleteAccentMark($string)
+    {
+        $string = str_replace(
+            array('Á', 'À', 'Â', 'Ä', 'á', 'à', 'ä', 'â', 'ª'),
+            array('A', 'A', 'A', 'A', 'a', 'a', 'a', 'a', 'a'),
+            $string
+        );
+     
+        //Reemplazamos la E y e
+        $string = str_replace(
+            array('É', 'È', 'Ê', 'Ë', 'é', 'è', 'ë', 'ê'),
+            array('E', 'E', 'E', 'E', 'e', 'e', 'e', 'e'),
+            $string
+        );
+     
+        //Reemplazamos la I y i
+        $string = str_replace(
+            array('Í', 'Ì', 'Ï', 'Î', 'í', 'ì', 'ï', 'î'),
+            array('I', 'I', 'I', 'I', 'i', 'i', 'i', 'i'),
+            $string
+        );
+     
+        //Reemplazamos la O y o
+        $string = str_replace(
+            array('Ó', 'Ò', 'Ö', 'Ô', 'ó', 'ò', 'ö', 'ô'),
+            array('O', 'O', 'O', 'O', 'o', 'o', 'o', 'o'),
+            $string
+        );
+     
+        //Reemplazamos la U y u
+        $string = str_replace(
+            array('Ú', 'Ù', 'Û', 'Ü', 'ú', 'ù', 'ü', 'û'),
+            array('U', 'U', 'U', 'U', 'u', 'u', 'u', 'u'),
+            $string
+        );
+    
+        //Reemplazamos la N, n, C y c
+        $string = str_replace(
+            array('Ñ', 'ñ', 'Ç', 'ç'),
+            array('N', 'n', 'C', 'c'),
+            $string
+        );
+        return strtolower($string);
+    }
+
+//--------------------------------------------------
+
     public function loadPanelAdministracion()
     {
         return view('panel.panelAdministracion');
@@ -157,7 +206,14 @@ class PanelAdministracion extends Controller
     }
 
     public function postArea(StoreArea $request) {
+        $originalRequest = new Request;
+        $originalRequest = $request->duplicate();
+        $request->nombre = $this->deleteAccentMark($request->nombre);
         $validated = $request->validated();
+        $request = $originalRequest;
+        $area = new Area;
+        $area->nombre = $request->nombre;
+        $area->save();
         return redirect('/panelAdministracion');
     }
     
