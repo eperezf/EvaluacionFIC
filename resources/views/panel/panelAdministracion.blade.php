7f6317f319edf84a0317a4d7ded346abb725379d
Nicolás Selman <nicolas.selman.25@gmail.com>
2020-10-31 14:31:37 -0300

    Modificacion view Panel Administracion
    Se modifica la vista principal de panel administración, separando las actividades de los demás formularios que son independientes a las actividades

--- resources/views/panel/panelAdministracion.blade.php
+++ resources/views/panel/panelAdministracion.blade.php
@@ -8,16 +8,6 @@
     <div class="col-auto">
       <div class="card mb-4">
         <div class="card-body" style="min-width: 25rem;">
-          <h4 class="card-title">Actividades</h4>
-          <p class="card-text">Agrega o modifica actividades</p>
-          <a class="btn btn-primary" href="{{ route('agregarActividad') }}">Agregar</a>
-          <a class="btn btn-secondary" href="{{ route('modificarActividad') }}">Modificar</a>
-        </div>
-      </div>
-    </div>
-    <div class="col-auto">
-      <div class="card mb-4">
-        <div class="card-body" style="min-width: 25rem;">
           <h4 class="card-title">Áreas</h4>
           <p class="card-text">Agrega o modifica áreas</p>
           <a class="btn btn-primary" href="{{ route('agregarArea') }}">Agregar</a>
@@ -25,6 +15,7 @@
         </div>
       </div>
     </div>
+
     <div class="col-auto">
       <div class="card mb-4">
         <div class="card-body" style="min-width: 25rem;">
@@ -35,6 +26,7 @@
         </div>
       </div>
     </div>
+
     <div class="col-auto">
       <div class="card mb-4">
         <div class="card-body" style="min-width: 25rem;">
@@ -45,86 +37,7 @@
         </div>
       </div>
     </div>
-    <div class="col-auto">
-      <div class="card mb-4">
-        <div class="card-body" style="min-width: 25rem;">
-          <h4 class="card-title">Cursos</h4>
-          <p class="card-text">Agrega o modifica cursos</p>
-          <a class="btn btn-primary" href="{{ route('agregarCurso') }}">Agregar</a>
-          <a class="btn btn-secondary" href="{{ route('modificarCurso') }}">Modificar</a>
-        </div>
-      </div>
-    </div>
-    <div class="col-auto">
-      <div class="card mb-4">
-        <div class="card-body" style="min-width: 25rem;">
-          <h4 class="card-title">Libros</h4>
-          <p class="card-text">Agrega o modifica libros</p>
-          <a class="btn btn-primary" href="{{ route('agregarLibro') }}">Agregar</a>
-          <a class="btn btn-secondary" href="{{ route('modificarLibro') }}">Modificar</a>
-        </div>
-      </div>
-    </div>
-    <div class="col-auto">
-      <div class="card mb-4">
-        <div class="card-body" style="min-width: 25rem;">
-          <h4 class="card-title">Licencias</h4>
-          <p class="card-text">Agrega o modifica licencias</p>
-          <a class="btn btn-primary" href="{{ route('agregarLicencia') }}">Agregar</a>
-          <a class="btn btn-secondary" href="{{ route('modificarLicencia') }}">Modificar</a>
-        </div>
-      </div>
-    </div>
-    <div class="col-auto">
-      <div class="card mb-4">
-        <div class="card-body" style="min-width: 25rem;">
-          <h4 class="card-title">Perfeccionamiento Docente</h4>
-          <p class="card-text">Agrega o modifica perfeccionamiento docente</p>
-          <a class="btn btn-primary" href="{{ route('agregarPerfeccionamientoDocente') }}">Agregar</a>
-          <a class="btn btn-secondary" href="{{ route('modificarPerfeccionamientoDocente') }}">Modificar</a>
-        </div>
-      </div>
-    </div>
-    <div class="col-auto">
-      <div class="card mb-4">
-        <div class="card-body" style="min-width: 25rem;">
-          <h4 class="card-title">Proyectos Concursables</h4>
-          <p class="card-text">Agrega o modifica proyectos concursables</p>
-          <a class="btn btn-primary" href="{{ route('agregarProyectoConcursable') }}">Agregar</a>
-          <a class="btn btn-secondary" href="{{ route('modificarProyectoConcursable') }}">Modificar</a>
-        </div>
-      </div>
-    </div>
-    <div class="col-auto">
-      <div class="card mb-4">
-        <div class="card-body" style="min-width: 25rem;">
-          <h4 class="card-title">Publicaciones</h4>
-          <p class="card-text">Agrega o modifica publicaciones</p>
-          <a class="btn btn-primary" href="{{ route('agregarPublicacion') }}">Agregar</a>
-          <a class="btn btn-secondary" href="{{ route('modificarPublicacion') }}">Modificar</a>
-        </div>
-      </div>
-    </div>
-    <div class="col-auto">
-      <div class="card mb-4">
-        <div class="card-body" style="min-width: 25rem;">
-          <h4 class="card-title">Spin Off</h4>
-          <p class="card-text">Agrega o modifica spin off</p>
-          <a class="btn btn-primary" href="{{ route('agregarSpinoff') }}">Agregar</a>
-          <a class="btn btn-secondary" href="{{ route('modificarSpinoff') }}">Modificar</a>
-        </div>
-      </div>
-    </div>
-    <div class="col-auto">
-      <div class="card mb-4">
-        <div class="card-body" style="min-width: 25rem;">
-          <h4 class="card-title">Subáreas</h4>
-          <p class="card-text">Agrega o modifica subáreas</p>
-          <a class="btn btn-primary" href="{{ route('agregarSubarea') }}">Agregar</a>
-          <a class="btn btn-secondary" href="{{ route('modificarSubarea') }}">Modificar</a>
-        </div>
-      </div>
-    </div>
+
     <div class="col-auto">
       <div class="card mb-4">
         <div class="card-body" style="min-width: 25rem;">
@@ -135,6 +48,101 @@
         </div>
       </div>
     </div>
+
+    <div class="col-auto">
+      <div class="card mb-4">
+        <div class="card-body" style="min-width: 25rem;">
+          <h4 class="card-title">Subáreas</h4>
+          <p class="card-text">Agrega o modifica subáreas</p>
+          <a class="btn btn-primary" href="{{ route('agregarSubarea') }}">Agregar</a>
+          <a class="btn btn-secondary" href="{{ route('modificarSubarea') }}">Modificar</a>
+        </div>
+      </div>
+    </div>
+  </div><br>
+  <h2>Actividades</h2><hr>
+  <div class="d-flex flex-wrap">
+    <div class="col-auto">
+      <div class="card mb-4">
+        <div class="card-body" style="min-width: 25rem;">
+          <h4 class="card-title">Cursos</h4>
+          <p class="card-text">Agrega o modifica cursos</p>
+          <a class="btn btn-primary" href="{{ route('agregarCurso') }}">Agregar</a>
+          <a class="btn btn-secondary" href="{{ route('modificarCurso') }}">Modificar</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-auto">
+      <div class="card mb-4">
+        <div class="card-body" style="min-width: 25rem;">
+          <h4 class="card-title">Libros</h4>
+          <p class="card-text">Agrega o modifica libros</p>
+          <a class="btn btn-primary" href="{{ route('agregarLibro') }}">Agregar</a>
+          <a class="btn btn-secondary" href="{{ route('modificarLibro') }}">Modificar</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-auto">
+      <div class="card mb-4">
+        <div class="card-body" style="min-width: 25rem;">
+          <h4 class="card-title">Licencias</h4>
+          <p class="card-text">Agrega o modifica licencias</p>
+          <a class="btn btn-primary" href="{{ route('agregarLicencia') }}">Agregar</a>
+          <a class="btn btn-secondary" href="{{ route('modificarLicencia') }}">Modificar</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-auto">
+      <div class="card mb-4">
+        <div class="card-body" style="min-width: 25rem;">
+          <h4 class="card-title">Perfeccionamiento Docente</h4>
+          <p class="card-text">Agrega o modifica perfeccionamiento docente</p>
+          <a class="btn btn-primary" href="{{ route('agregarPerfeccionamientoDocente') }}">Agregar</a>
+          <a class="btn btn-secondary" href="{{ route('modificarPerfeccionamientoDocente') }}">Modificar</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-auto">
+      <div class="card mb-4">
+        <div class="card-body" style="min-width: 25rem;">
+          <h4 class="card-title">Proyectos Concursables</h4>
+          <p class="card-text">Agrega o modifica proyectos concursables</p>
+          <a class="btn btn-primary" href="{{ route('agregarProyectoConcursable') }}">Agregar</a>
+          <a class="btn btn-secondary" href="{{ route('modificarProyectoConcursable') }}">Modificar</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-auto">
+      <div class="card mb-4">
+        <div class="card-body" style="min-width: 25rem;">
+          <h4 class="card-title">Publicaciones</h4>
+          <p class="card-text">Agrega o modifica publicaciones</p>
+          <a class="btn btn-primary" href="{{ route('agregarPublicacion') }}">Agregar</a>
+          <a class="btn btn-secondary" href="{{ route('modificarPublicacion') }}">Modificar</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-auto">
+      <div class="card mb-4">
+        <div class="card-body" style="min-width: 25rem;">
+          <h4 class="card-title">Spin Off</h4>
+          <p class="card-text">Agrega o modifica spin off</p>
+          <a class="btn btn-primary" href="{{ route('agregarSpinoff') }}">Agregar</a>
+          <a class="btn btn-secondary" href="{{ route('modificarSpinoff') }}">Modificar</a>
+        </div>
+      </div>
+    </div>
+
+    
+
+    
+
     <div class="col-auto">
       <div class="card mb-4">
         <div class="card-body" style="min-width: 25rem;">
@@ -145,6 +153,7 @@
         </div>
       </div>
     </div>
+
     <div class="col-auto">
       <div class="card mb-4">
         <div class="card-body" style="min-width: 25rem;">
@@ -155,6 +164,7 @@
         </div>
       </div>
     </div>
+
     <div class="col-auto">
       <div class="card mb-4">
         <div class="card-body" style="min-width: 25rem;">
@@ -165,6 +175,7 @@
         </div>
       </div>
     </div>
+
   </div>
 
 
