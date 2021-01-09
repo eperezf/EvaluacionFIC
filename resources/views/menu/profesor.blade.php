3232554c06d7bd10c73cee342190835b81577ed6
Dayhan Vásquez <dvasquez@alumnos.uai.cl>
2021-01-09 16:36:25 -0300

    Arreglos y botones
    Arreglé la vista del menú del profesor y le agregué botones en Docencia. Aun no sirven.

--- resources/views/menu/profesor.blade.php
+++ resources/views/menu/profesor.blade.php
@@ -2,14 +2,25 @@
 
 @section('title', 'Inicio')
 @section('contenido')
-  <h1>Bienvenido/a {{ $nombre }}.</h1><br>
-  
-  <h2>Docencia</h2><hr>
-    <p> Aqui debe ir las actividades del profesor de cursos, tutorias y perfeccionamiento docente </p><br>
-  <h2>Administración Académica</h2><hr>
-    <p>Las actividades de cargos directivos</p><br>
-  <h2>Vinculación con el medio</h2><hr>
-  <h2>Investigación</h2><hr>
-    <p>Las actividades de publicacion,libros, proyectos concursables,etc.</p><br>
-  <h2>Otros</h2><hr>
+  <h1>Mi información</h1><br>
+  <div id="información">
+    <section id="docencia">
+      <h3>Docencia</h3><hr>
+      <a class="btn btn-secondary mr-2 my-1" href="{{ "" }}">Ver mis cursos</a>
+      <a class="btn btn-secondary mr-2 my-1" href="{{ "" }}">Ver mis tutorías</a>
+      <a class="btn btn-secondary mr-2 my-1" href="{{ "" }}">Ver mis perfeccionamientos docentes</a>
+    </section><br>
+    <section id="administracion">
+      <h3>Administración Académica</h3><hr>
+    </section><br>
+    <section id="vinculacion">
+      <h3>Vinculación con el medio</h3><hr>
+    </section><br>
+    <section id="investigacion">
+      <h3>Investigación</h3><hr>
+    </section><br>
+    <section id="otros">
+      <h3>Otros</h3><hr>
+    </section><br>
+  </div>
 @endsection
