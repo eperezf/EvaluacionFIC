74a146620a3988f17ddc23a81b4bf4a126a10113
Nicolás Selman <nicolas.selman.25@gmail.com>
2021-01-14 20:01:57 -0300

    Se agrega cargo Director de área

--- database/seeds/CargoSeeder.php
+++ database/seeds/CargoSeeder.php
@@ -22,29 +22,36 @@ class CargoSeeder extends Seeder
                 'updated_at' => Carbon::now()
             ],
             [
-                'nombre' => 'Director de Investigación',
+                'nombre' => 'Director de investigación',
                 'idtipoactividad' => App\Tipoactividad::where('nombre', 'Investigacion')->get('id')[0]->id,
                 'peso' => '5', //No se qué peso ponerle
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now()
             ],
             [
-                'nombre' => 'Director Ejecutivo de Investigación',
+                'nombre' => 'Director ejecutivo de investigación',
                 'idtipoactividad' => App\Tipoactividad::where('nombre', 'Investigación')->get('id')[0]->id,
                 'peso' => '5', //No se qué peso ponerle
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now()
             ],
             [
-                'nombre' => 'Director de Docencia',
+                'nombre' => 'Director de docencia',
                 'idtipoactividad' => App\Tipoactividad::where('nombre', 'Asignatura')->get('id')[0]->id,
                 'peso' => '3', //No se qué peso ponerle
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now()
             ],
             [
-                'nombre' => 'Subdirector de Docencia',
+                'nombre' => 'Subdirector de docencia',
                 'idtipoactividad' => App\Tipoactividad::where('nombre', 'Asignatura')->get('id')[0]->id,
+                'peso' => '2', //No se qué peso ponerle
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'nombre' => 'Director de área',
+                'idtipoactividad' => App\Tipoactividad::where('nombre', 'Área')->get('id')[0]->id,
                 'peso' => '2', //No se qué peso ponerle
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now()
