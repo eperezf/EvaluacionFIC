0d6ccbf2567f42000555bad2313163725d14e886
Eduardo Pérez <eduardojoseperezf@gmail.com>
2021-01-13 23:04:45 -0300

    Selects de tipo de actividad y cargo

--- public/js/addCargo.js
+++ public/js/addCargo.js
@@ -1,4 +1,16 @@
-
-function getCargos() {
-    
-}
+$(document).ready(()=>{
+  $('#tipoActividad').on('change', function (){
+    console.log($(this).val());
+    var idTipoActividad = $(this).val()
+    var request = new Request('/api/getCargoTipoActividad/'+idTipoActividad)
+    fetch(request).then((response)=>{
+      return response.json();
+    }).then((response)=>{
+      console.log(response);
+      $('#cargo').empty()
+      response.forEach((item, i) => {
+        $('#cargo').append('<option value="'+item.id+'">'+item.nombre+'</option>').removeAttr("disabled");
+      });
+    })
+  })
+})
--- resources/views/menu/administrador/perfilDocenteCargo.blade.php
+++ resources/views/menu/administrador/perfilDocenteCargo.blade.php
@@ -10,10 +10,10 @@
     <button class="btn btn-primary col-1">Guardar</button>
   </div><hr>
   <div id="formData">
-    <div id="tipoActividad" class="form-group row">
+    <div class="form-group row">
       <label for="tipoActividad" class="col-sm-3">Seleccione un tipo de actividad</label>
       <div class="col-sm-5">
-        <select name="tipoActividad" id="tipoSelect" class="form-control">
+        <select name="tipoActividad" id="tipoActividad" class="form-control">
           <option disabled selected>Seleccione un tipo de actividad</option>
           @foreach ($tipoActividades as $tipo)
             <option value="{{ $tipo->id }}">{{ $tipo->nombre }}</option>
@@ -21,8 +21,15 @@
         </select>
       </div>
     </div>
-    <div id="cargo">
-      {{-- Respuesta JavaScript --}}
+    <div>
+      <div class="form-group row">
+        <label for="tipoActividad" class="col-sm-3">Seleccione un tipo de actividad</label>
+        <div class="col-sm-5">
+          <select name="cargo" id="cargo" class="form-control" disabled>
+            <option disabled selected>Seleccione un cargo</option>
+          </select>
+        </div>
+      </div>
     </div>
   </div>
   <input type="hidden" value="{{ $usuario->id }}" name="userId">
