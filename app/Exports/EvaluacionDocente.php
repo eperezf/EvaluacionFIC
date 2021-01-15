646fdf2c907740232bcda7f3f4e7af90363a03d3
José Ignacio Miquel <jomiquel@alumnos.uai.cl>
2021-01-14 21:22:37 -0300

    Fila de encabezados en negrita

--- app/Exports/EvaluacionDocente.php
+++ app/Exports/EvaluacionDocente.php
@@ -9,9 +9,11 @@ use Maatwebsite\Excel\Concerns\FromQuery;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
 
-class EvaluacionDocente implements FromQuery, WithHeadings, ShouldAutoSize, WithMapping
+class EvaluacionDocente implements FromQuery, WithHeadings, ShouldAutoSize, WithMapping, WithStyles
 {
     public function headings(): array
     {
@@ -21,6 +23,13 @@ class EvaluacionDocente implements FromQuery, WithHeadings, ShouldAutoSize, With
             'Apellido Materno',
             'Curso',
             'Nota'
+        ];
+    }
+
+    public function styles(Worksheet $sheet)
+    {
+        return [
+            1 => ['font' => ['bold' => true]]
         ];
     }
 
