a69fa0d09566a85c165a1519f9f62da475d0e42b
Dayhan Vásquez <dvasquez@alumnos.uai.cl>
2021-01-27 18:00:14 -0300

    Arreglo del buscador
    aun falta ver la repetición.

--- app/Http/Controllers/MenuDirectorDocencia.php
+++ app/Http/Controllers/MenuDirectorDocencia.php
@@ -39,9 +39,10 @@ class MenuDirectorDocencia extends Controller
         $idProfesor = Cargo::where('nombre', 'Profesor')->get()[0]->id;
 
         /* Obtenemos el los usuarios que tengan el cargo de profesor y coincidan con la letra */
-        $usuarios = DB::table('user')->join('user_actividad', 'user.id', '=', 'user_actividad.iduser')
+        $usuarios = DB::table('user')
+        ->join('user_actividad', 'user.id', '=', 'user_actividad.iduser')
         ->where('user_actividad.idcargo', 'LIKE', $idProfesor)
-        ->where('user.nombres', 'LIKE', $letra.'%')
+        ->where('user.apellidoPaterno', 'LIKE', $letra.'%')
         ->select('user.id','user.nombres','user.apellidoPaterno', 'user.apellidoMaterno')
         ->get();
 
@@ -54,12 +55,15 @@ class MenuDirectorDocencia extends Controller
         $menus = Helper::getMenuOptions(Auth::user()->id);
         
         //Obenemos los usuarios que calcen con el valor del input ingresado
-        $usuarios = User::where(DB::raw("CONCAT_WS(' ', user.nombres, user.apellidoPaterno, user.apellidoMaterno)"), 'LIKE', '%'.$request->search.'%')
+        $usuarios = DB::table('user')
+        ->join('user_actividad', 'user.id', '=', 'user_actividad.iduser')
+        ->where('user_actividad.idcargo', '=', Cargo::where('nombre', 'Profesor')->get()[0]->id)
+        ->where(DB::raw("CONCAT_WS(' ', user.nombres, user.apellidoPaterno, user.apellidoMaterno)"), 'LIKE', '%'.$request->search.'%')
         ->get([
-            'id',
-            'nombres',
-            'apellidoPaterno',
-            'apellidoMaterno'
+            'user.id',
+            'user.nombres',
+            'user.apellidoPaterno',
+            'user.apellidoMaterno'
         ]);
         return view('menu.directorDocencia.buscador', ['nombre' => $nombre, 'usuarios' => $usuarios, 'menus' => $menus]);
     }
